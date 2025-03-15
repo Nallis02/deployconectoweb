1d35a855fc9e93e77436ad1524ebd3de7c799f3a
nallis02 <nallive14@gmail.com>
2025-03-15 13:22:17 -0500

    cambios ok

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "https://nallis02.github.io/deployconectoweb/",
+  base: "/deployconectoweb/",
 });
